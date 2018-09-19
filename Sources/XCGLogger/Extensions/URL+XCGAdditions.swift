6e123e9772175b14f79e31e00476eea839c34584
Kerry Knight <kerry@kerryknight.io>
2018-09-19 10:53:42 -0400

    Update for Xcode 10 and iOS 12 compatibility

--- Sources/XCGLogger/Extensions/URL+XCGAdditions.swift
+++ Sources/XCGLogger/Extensions/URL+XCGAdditions.swift
@@ -11,28 +11,32 @@
 import Foundation
 
 extension URL {
-
+    
     /// Get extended attribute.
     func extendedAttribute(forName name: String) throws -> Data? {
         let data: Data? = try self.withUnsafeFileSystemRepresentation { (fileSystemPath: (UnsafePointer<Int8>?)) -> Data? in
             // Determine attribute size
             let length = getxattr(fileSystemPath, name, nil, 0, 0, 0)
             guard length >= 0 else { return nil }
-
+            
             // Create buffer with required size
             var data = Data(count: length)
-
+            var mutableCopy = data
+            
             // Retrieve attribute
-            let result = data.withUnsafeMutableBytes {
+            let result = mutableCopy.withUnsafeMutableBytes {
                 getxattr(fileSystemPath, name, $0, data.count, 0, 0)
             }
             guard result >= 0 else { throw URL.posixError(errno) }
+            
+            data = mutableCopy
+            
             return data
         }
-
+        
         return data
     }
-
+    
     /// Set extended attribute.
     func setExtendedAttribute(data: Data, forName name: String) throws {
         try self.withUnsafeFileSystemRepresentation { fileSystemPath in
@@ -42,7 +46,7 @@ extension URL {
             guard result >= 0 else { throw URL.posixError(errno) }
         }
     }
-
+    
     /// Remove extended attribute.
     func removeExtendedAttribute(forName name: String) throws {
         try self.withUnsafeFileSystemRepresentation { fileSystemPath in
@@ -50,22 +54,25 @@ extension URL {
             guard result >= 0 else { throw URL.posixError(errno) }
         }
     }
-
+    
     /// Get list of all extended attributes.
     func listExtendedAttributes() throws -> [String] {
         let list = try self.withUnsafeFileSystemRepresentation { (fileSystemPath: (UnsafePointer<Int8>?)) -> [String] in
             let length = listxattr(fileSystemPath, nil, 0, 0)
             guard length >= 0 else { throw URL.posixError(errno) }
-
+            
             // Create buffer with required size
             var data = Data(count: length)
-
+            var mutableCopy = data
+            
             // Retrieve attribute list
-            let result = data.withUnsafeMutableBytes {
+            let result = mutableCopy.withUnsafeMutableBytes {
                 listxattr(fileSystemPath, $0, data.count, 0)
             }
             guard result >= 0 else { throw URL.posixError(errno) }
-
+            
+            data = mutableCopy
+            
             // Extract attribute names
             let list = data.split(separator: 0).flatMap {
                 String(data: Data($0), encoding: .utf8)
@@ -74,7 +81,7 @@ extension URL {
         }
         return list
     }
-
+    
     /// Helper function to create an NSError from a Unix errno.
     private static func posixError(_ err: Int32) -> NSError {
         return NSError(domain: NSPOSIXErrorDomain, code: Int(err), userInfo: [NSLocalizedDescriptionKey: String(cString: strerror(err))])
